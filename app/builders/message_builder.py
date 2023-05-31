dbdd1c3684cc5fce559adbf7ca33e78302d11fad
Kirill Khalevin <kirkhal0909@gmail.com>
2023-05-31 18:51:04 +0300

    ✓ dividends

--- app/builders/message_builder.py
+++ app/builders/message_builder.py
@@ -1,3 +1,5 @@
+from app.helpers.dictionaries import first_key
+
 class Message():
     @staticmethod
     def indexes_to_text(indexes):
@@ -12,7 +14,14 @@ class Message():
         message += '\n\n'
         for ticker in data['tickers']:
           data_ticker = data['tickers'][ticker]
-          message += "   {}{}".format(ticker, '                     дивиденды: {}% ({})\n'.format(data_ticker.get('percent'), data_ticker.get('last_buy_day')) if data_ticker.get('percent') else '\n')
+          if data_ticker.get('percent'):
+            days = first_key(data_ticker['changes'])
+            dividend_value = round(data_ticker['changes'][days][1] / 100 * data_ticker.get('percent'), 2)
+            mark_highlight = '✓' if dividend_value > 400_000_000 else ''
+          else:
+            dividend_value = 0
+            mark_highlight = ''
+          message += "   {}{}".format(ticker, ' {}                   дивиденды: {}% ({}) {}\n'.format(mark_highlight, data_ticker.get('percent'), data_ticker.get('last_buy_day'), Message.format_long_number(dividend_value)) if data_ticker.get('percent') else '\n')
           for day in data_ticker['changes']:
             message += "    {}: {}% {}\n".format(day, data_ticker['changes'][day][0], Message.format_long_number(data_ticker['changes'][day][1]))
         message += '\n-----------------------'
