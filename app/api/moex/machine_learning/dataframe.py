bab06eaeb05de5f7913eb6ad8af938e9f4a3db44
Kirill Khalevin <kirkhal0909@gmail.com>
2023-06-14 16:27:07 +0300

    normalize volumes, values, capitalization

--- app/api/moex/machine_learning/dataframe.py
+++ app/api/moex/machine_learning/dataframe.py
@@ -5,7 +5,8 @@ class Dataframe():
   def normalize(self, dataframe):
     dataframe = self.remove_index_and_data(dataframe)
     dataframe = self.remove_top_nan(dataframe)
-    dataframe = self.classify(dataframe)
+    dataframe = self.classify_prices(dataframe)
+    dataframe = self.classify_volumes(dataframe)
     dataframe = self.add_tomorrow_close(dataframe)
     return dataframe[:-1].dropna()
 
@@ -21,7 +22,7 @@ class Dataframe():
     dataframe['tomorrow_close'] = dataframe.close.shift(-1)
     return dataframe
 
-  def classify(self, dataframe):
+  def classify_prices(self, dataframe):
     dataframe = dataframe.copy()
     for column in dataframe.columns:
       if 'close' in column and column != 'tomorrow_close':
@@ -31,15 +32,24 @@ class Dataframe():
         col_high = list(dataframe[interpolate_column.format('high')])
         col_close = list(dataframe[column])
         for pos in range(len(dataframe) - 1, 0, -1):
-          if col_close[pos] != np.nan:
-            col_open[pos] = self.normalize_percent(col_close[pos], col_open[pos])
-            col_low[pos] = self.normalize_percent(col_close[pos], col_low[pos])
-            col_high[pos] = self.normalize_percent(col_close[pos], col_high[pos])
-            col_close[pos] = self.normalize_percent(col_close[pos], col_close[pos - 1])
+          col_open[pos] = self.normalize_percent(col_close[pos], col_open[pos])
+          col_low[pos] = self.normalize_percent(col_close[pos], col_low[pos])
+          col_high[pos] = self.normalize_percent(col_close[pos], col_high[pos])
+          col_close[pos] = self.normalize_percent(col_close[pos], col_close[pos - 1])
         dataframe[interpolate_column.format('open')] = np.array(col_open)
         dataframe[interpolate_column.format('low')] = np.array(col_low)
         dataframe[interpolate_column.format('high')] = np.array(col_high)
         dataframe[column] = np.array(col_close)
+    return dataframe
+
+  def classify_volumes(self, dataframe):
+    dataframe = dataframe.copy()
+    for column in dataframe.columns:
+      if any(substring in column for substring in ['volume', 'value', 'capitalization']):
+        values = [ int(value) if not math.isnan(value) else value for value in list(dataframe[column])]
+        dataframe["{}_e".format(column)] = np.array([ len(str(value)) - 1 for value in values])
+        rounded_prefixes = [ round(int(str(int(value)).ljust(3, '0')[:3]) / 100) if not math.isnan(value) else value for value in list(dataframe[column])]
+        dataframe[column] = np.array(rounded_prefixes)
     return dataframe
 
   def normalize_percent(self, value1, value2, step = 0.2):
