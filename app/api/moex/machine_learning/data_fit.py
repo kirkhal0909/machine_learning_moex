c08e8e81daa530e8607216fe7d8ba7fe7ed6f598
Kirill Khalevin <kirkhal0909@gmail.com>
2023-06-15 00:43:46 +0300

    get_x_y

--- app/api/moex/machine_learning/data_fit.py
+++ app/api/moex/machine_learning/data_fit.py
@@ -1,2 +1,9 @@
+import numpy as np
+
 class DataFit():
-    None
+  def get_x_y(dataframe, x_range=30):
+    dataframe = dataframe.copy()
+    y = np.array(dataframe.tomorrow_close[x_range - 1:])
+    dataframe = dataframe.drop(columns=['tomorrow_close'])
+    x = np.array([windowed for windowed in dataframe.rolling(window=x_range)][x_range - 1:])
+    return x, y
