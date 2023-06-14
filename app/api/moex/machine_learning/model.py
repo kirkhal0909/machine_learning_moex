a9db7968fd550751696a559120e024e4c4930697
Kirill Khalevin <kirkhal0909@gmail.com>
2023-06-15 02:57:19 +0300

    save full model; load model to continue fit

--- app/api/moex/machine_learning/model.py
+++ app/api/moex/machine_learning/model.py
@@ -2,13 +2,17 @@ from keras.models import Sequential
 from keras.layers import Dense, LSTM, Dropout
 import keras
 from keras.callbacks import ModelCheckpoint
+import os
 
 class Model():
   def __init__(self, model_version = 1) -> None:
-    self.__model_path__ = 'models/moex.h5'
+    self.__model_path__ = 'models/moex_v{}.h5'.format(model_version)
     self.__model_version__ = model_version
 
   def model(self, input_shape):
+    if os.path.exists(self.__model_path__):
+      return keras.models.load_model(self.__model_path__)
+
     model = Sequential()
 
     if self.__model_version__ == 1:
@@ -24,17 +28,17 @@ class Model():
       model.add(Dropout(0.25))
       model.add(Dense(units=1, activation='linear'))
 
-    model.compile(optimizer = keras.optimizers.Adam(learning_rate=0.01), loss='mean_squared_error')
     return model
 
-  def fit(self, x_train, y_train, batch_size = 512, epochs = 64):
+  def fit(self, x_train, y_train, batch_size = 1024, epochs = 64):
     try:
       self.__model__
     except AttributeError:
       self.__model__ = self.model(x_train[0].shape)
+    self.__model__.compile(optimizer = keras.optimizers.Adam(learning_rate=0.01), loss='mean_squared_error')
     # es = EarlyStopping(monitor='val_loss', min_delta=1e-10, patience=10, verbose=1)
     # rlr = ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=10, verbose=1)
-    mcp = ModelCheckpoint(filepath=self.__model_path__, monitor='val_loss', verbose=1, save_best_only=True, save_weights_only=True)
+    mcp = ModelCheckpoint(filepath=self.__model_path__, monitor='val_loss', verbose=1, save_best_only=True, save_weights_only=False)
     self.__model__.fit(
       x_train,
       y_train,
