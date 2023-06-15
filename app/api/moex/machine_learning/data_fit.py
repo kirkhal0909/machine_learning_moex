8dcb9ba4ed475dc0e6eef468d6ac9814d6eaed98
Kirill Khalevin <kirkhal0909@gmail.com>
2023-06-15 18:20:23 +0300

    scale y_data

--- app/api/moex/machine_learning/data_fit.py
+++ app/api/moex/machine_learning/data_fit.py
@@ -1,6 +1,19 @@
 import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+import joblib
+import os
 
 class DataFit():
+  def __init__(self) -> None:
+    self.__scale_range__ = (-10, 10)
+    self.__scaler_path__ = 'models/scaler_{}_{}.pkl'.format(self.__scale_range__[0], self.__scale_range__[1])
+    self.scaler = self.load_or_create_scaler()
+
+  def load_or_create_scaler(self):
+    if os.path.exists(self.__scaler_path__):
+      return joblib.load(self.__scaler_path__)
+    return MinMaxScaler(feature_range=self.__scale_range__)
+
   def get_x_y(self, dataframes, x_range=30):
     y = []
     x = []
@@ -9,4 +22,14 @@ class DataFit():
       y += list(dataframe.tomorrow_close[x_range - 1:])
       dataframe = dataframe.drop(columns=['tomorrow_close'])
       x += [windowed for windowed in dataframe.rolling(window=x_range)][x_range - 1:]
-    return np.array(x), np.array(y)
+    return np.array(x), np.array(y).reshape(len(y), 1)
+
+  def scale(self, data_series):
+    self.scaler = self.scaler.fit(data_series)
+    scaled = self.scaler.transform(data_series)
+    joblib.dump(self.scaler, self.__scaler_path__)
+
+    return scaled
+
+  def unscale(self, data_series):
+    return self.scaler.inverse_transform(data_series)
--- app/api/moex/machine_learning/ml.py
+++ app/api/moex/machine_learning/ml.py
@@ -4,13 +4,14 @@ from app.api.moex.machine_learning.model import Model
 from app.api.moex.machine_learning.dataframe import Dataframe
 
 class ML():
-  def __init__(self, client, parser) -> None:
+  def __init__(self, client, parser, model_version = 1) -> None:
     self.data = DataMoex(client, parser)
     self.data_fit = DataFit()
-    self.model = Model()
+    self.model = Model(model_version)
     self.dataframe = Dataframe(self.data)
 
   def fit(self, ticker='ALL'):
     x, y = self.data_fit.get_x_y(self.dataframe.get_dataframes(ticker))
-    model = self.model.fit(x, y)
+    y_scaled = self.data_fit.scale(y)
+    model = self.model.fit(x, y_scaled)
     return x, y, model
